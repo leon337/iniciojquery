cd99cd2d322371c95815ad4cce1fb5105b111669
Leandro Carlos <leonpcsn@gmail.com>
2021-03-30 20:29:18 -0300

    Dimensoes Dinamicamente

---
+++ js/dimensoesdinamicamente.js
@@ -0,0 +1,23 @@
+$(function(){
+ 
+    // PEGANDO O VALOR DA LARGURA DO ELEMENTO
+    $(".box-div").width();
+    // SETANDO O VALOR DA LARGURA DO ELEMENTO
+    $(".box-div").width(400);
+    
+    // PEGANDO O VALOR DA LARGURA DO ELEMENTO
+    $(".box-div").height();
+    // SETANDO O VALOR DA LARGURA DO ELEMENTO
+    $(".box-div").height(400);
+
+    console.log("Width: "+$('.box-div').width());
+    console.log("Inner Width: "+$('.box-div').innerWidth());
+    console.log("Outer Width: "+$('.box-div').outerWidth());
+    
+
+    console.log("***********")
+
+    console.log("Heigth: "+$('.box-div').height());
+    console.log("Inner Heigth: "+$('.box-div').innerHeight());
+    console.log("Outer Hiegth: "+$('.box-div').outerHeight());
+})
