5593d08c133f49e4eab5a8d840cddfa2f01bb1d7
Leandro Carlos <leonpcsn@gmail.com>
2021-03-30 17:29:33 -0300

    Finalizando o basico do basico em Jquery

---
+++ js/teste.js
@@ -0,0 +1,20 @@
+$(function(){
+// EVENTO DE SCROLL DA TELA
+$(window).scroll(function(){
+      
+})
+
+// EVENDO DE RESIZE DA TELA
+$(window).resize(function(){
+    console.log("Minha tela está sendo redimensionada")
+    
+    $("body").css("background-color", "blue")
+})
+
+
+
+
+
+
+
+})
